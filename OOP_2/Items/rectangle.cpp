40ece94ca6c19037b59dad2f437e325589f88b51
VanuhiMkrtchyan <vanuhi.mkrtchyan2003@gmail.com>
2023-11-25 18:06:02 +0400

    25.11

--- OOP_2/Items/rectangle.cpp
+++
@@ -1,7 +0,0 @@
-#include <iosteram>
-
-#include "rectangle.hpp"
-
-void Rectangle:draw(){
-    std::cout << "Rectangle is drawn with width: " << width <<"  height: " << height << " in Coordinates: " << x << "  " << y << std::endl;
-}
--- OOP_2/main.cpp
+++
@@ -1,6 +0,0 @@
-#include "Controller/controller.hpp"
-
-int main(int argc, char* argv[]){
-    Controller controller;
-    controller.run(argc, argv);
-}
